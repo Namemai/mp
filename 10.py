02be2c49f8b68d072007c76943b3801f9c6550d8
Namemai <40319864+Namemai@users.noreply.github.com>
2019-03-19 18:44:34 +0000

    Update 10.py

--- 10.py
+++ 10.py
@@ -11,15 +11,8 @@ import time, random, sys, json, codecs, threading, glob, re, string, os, request
 #==============================================================================#
 botStart = time.time()
 #==============================================================================#
-line = LINE()
+line = LINE("EDqRVUcShkwZwPdpqv37.MNDJ+FLNS0xznCFGA2mQ1W.ImVOCE3qtyYJL9XfvKsiNqy+/ggJzGaDkTBll7Dztx0)
 line.log("Auth Token : " + str(line.authToken))
-channel = LineChannel(line)
-line.log("Channel Access Token : " + str(channel.channelAccessToken))
-lineProfile = line.getProfile()
-lineSettings = line.getSettings()
-mid = line.getProfile().mid
-
-responsename = cl.getProfile().displayName
 # Assist
 ki = LINE("EDkRobZOqw8xtawF38M4.+0VwFLVgQ2j2cX+3JyNF5a.wKILaaflA5oamztZbovmR+6jR6vOYD8o1PL8anw7DPU")
 ki.log("Auth Token : " + str(ki.authToken))
